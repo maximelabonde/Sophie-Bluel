697b1bae4b4878432e3e8d085ff24cbc8c562a65
Maxgazore <167684044+Maxgazore@users.noreply.github.com>
2024-12-13 17:20:23 +0100

    Ajout de la fonctionnalité pour supprimer les projets.

--- FrontEnd/assets/scripts/works.js
+++ FrontEnd/assets/scripts/works.js
@@ -1,3 +1,5 @@
+const token = localStorage.getItem("token");
+
 async function worksGet() {
     const works = await fetch("http://localhost:5678/api/works").then(
         (response) => response.json()
@@ -29,6 +31,83 @@ async function modalCategoriesGet() {
 }
 
 modalCategoriesGet();
+
+async function worksModalGet() {
+    if (!token) {
+        console.error("Token non trouvé");
+        return;
+    }
+    const works = await fetch("http://localhost:5678/api/works").then(
+        (response) => response.json()
+    );
+    for (let i = 0; i < works.length; i++) {
+        const worksElement = document.createElement("figure");
+        const imgElement = document.createElement("img");
+        const iconElement = document.createElement("i");
+        imgElement.src = works[i].imageUrl;
+        worksElement.id = works[i].id;
+        iconElement.classList.add("fa-solid", "fa-trash-can");
+        modalGallery.appendChild(worksElement);
+        worksElement.appendChild(imgElement);
+        worksElement.appendChild(iconElement);
+
+        
+        iconElement.addEventListener("click", async function () {
+            console.log(worksElement.id);
+            const response = await fetch(
+                "http://localhost:5678/api/works/" + worksElement.id,
+                {
+                    method: "DELETE",
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                }
+            );
+            if (!response.ok) {
+                throw new Error(
+                    `Erreur : ${response.status} ${response.statusText}`
+                );
+            } else {
+                worksElement.remove();
+            }
+        });
+    }
+}
+
+worksModalGet();
+
+async function addWork(e) {
+    e.preventDefault();
+    if (!token) {
+        console.error("Token non trouvé");
+        return;
+    }
+    const formData = new FormData(e.target);
+    try {
+        const response = await fetch("http://localhost:5678/api/works", {
+            method: "POST",
+            body: formData,
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+        if (!response.ok) {
+            throw new Error(
+                `Erreur : ${response.status} ${response.statusText}`
+            );
+        }
+        const data = await response.json();
+        if (data) {
+            console.log(data);
+        }
+    } catch (error) {
+        console.error("Erreur lors de l'ajout de l'image:", error);
+    }
+}
+
+modalForm.addEventListener("submit", (e) => {
+    addWork(e);
+});
 
 modalEditBtn.addEventListener("click", () => {
     modal.style.display = "flex";
